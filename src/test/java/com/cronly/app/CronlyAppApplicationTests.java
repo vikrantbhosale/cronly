be28fbd887abfa4d52295ac171c25c719f00ff46
Vikrant Bhosale <vikrant_bhosale@outlook.com>
2019-12-24 11:41:48 +0530

    cron inito

---
+++ src/test/java/com/cronly/app/CronlyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cronly.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronlyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
